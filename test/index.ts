d04db3b2f2dbc5b2873ee0e9338e994548ff376e
sunyubo <sunyubo34@gmail.com>
2020-03-22 10:09:44 +0800

    docs: add demo

--- test/index.ts
+++ test/index.ts
@@ -2,3 +2,14 @@ import Mirai from 'mirai-node-typescript'
 
 const mirai = new Mirai()
 
+// 监听事件
+mirai.onListenMessage({
+  msgCallback(msg) {
+    // do something
+    console.log(msg)
+  },
+  eventCallback(event) {
+    // do something
+    console.log(event)
+  }
+})
--- test/test1.js
+++ test/test1.js
@@ -1,3 +1,15 @@
-const Mirai = require('../lib').default
+const Mirai = require('mirai-node-typescript').default
 
 const mirai = new Mirai()
+
+// 监听事件
+mirai.onListenMessage({
+  msgCallback(msg) {
+    // do something
+    console.log(msg)
+  },
+  eventCallback(event) {
+    // do something
+    console.log(event)
+  }
+})
